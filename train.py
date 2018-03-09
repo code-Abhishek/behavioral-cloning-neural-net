5eb35af877ef047256ad4de578340bc226ef2066
Daniel Hunter <officialdanielhunter@gmail.com>
2018-03-09 22:39:10 +0000

    Added preprocessing and LeNet arch

--- train.py
+++ train.py
@@ -1,6 +1,10 @@
 import csv
 import cv2
 import numpy as np
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 lines = []
 with open('data/driving_log.csv') as file:
@@ -22,11 +26,17 @@ for line in lines:
 X_train = np.array(images) 
 y_train = np.array(measurements) 
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
+model.add(Convolution2D(6,5,5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
