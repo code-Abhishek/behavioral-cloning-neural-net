5b05ba7e53385ccea7d9cee691c2ec75c4c2a348
Daniel Hunter <officialdanielhunter@gmail.com>
2018-03-10 06:46:36 +0000

    Add dropout and image mirroring data augmentation

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
@@ -21,11 +21,16 @@ for line in lines:
     current_path = './data/IMG/' + filename
     image = cv2.imread(current_path)
     images.append(image)
-    measurements.append(float(line[3]))
+    measurement = float(line[3])
+    measurements.append(measurement)
+    
+    flipped_image = cv2.flip(image, 1)
+    flipped_measurement = float(measurement) * -1.0
+    images.append(flipped_image)
+    measurements.append(flipped_measurement)
     
 X_train = np.array(images) 
 y_train = np.array(measurements) 
-
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
@@ -36,11 +41,12 @@ model.add(Convolution2D(6,5,5, activation='relu'))
 model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dense(120))
+model.add(Dropout(0.5))
 model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=4)
 
 model.save('steering.h5')
 
