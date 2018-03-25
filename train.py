42714117b38ac493a4f0a77ee834d75b91fb3719
Daniel Hunter <officialdanielhunter@gmail.com>
2018-03-25 01:09:43 +0000

    Update network architecture based on Nvidia paper

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
-correction = 0.5 # degrees? Probably too small
+correction = 0.25 
 batch_size = 32
 
 def path_to_filename(path):
@@ -76,7 +76,7 @@ for CSV in CSVs:
         tmp_lines = []
         for line in reader:
             tmp_lines.append(line)
-        print('Reading file', str(CSV), 'it has', len(tmp_lines), 'lines!')
+        print('Reading file', str(CSV), 'it has', len(tmp_lines), 'lines')
         tmp_lines = tmp_lines[1:] # discard first line as it is just titles, not data
         lines += tmp_lines
 
@@ -90,16 +90,19 @@ train_gen = generator(train, batch_size)
 val_gen = generator(val, batch_size)
 
 model = Sequential()
+# Normalize and crop the images
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
-model.add(Convolution2D(6, (5, 5), activation='relu'))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6, (5, 5), activation='relu'))
-model.add(MaxPooling2D())
+
+model.add(Convolution2D(24, (5, 5), strides=2, activation='relu'))
+model.add(Convolution2D(36, (5, 5), strides=2, activation='relu'))
+model.add(Convolution2D(48, (5, 5), strides=2, activation='relu'))
+model.add(Convolution2D(64, (3, 3), activation='relu'))
+model.add(Convolution2D(64, (3, 3), activation='relu'))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dropout(0.5))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
@@ -109,4 +112,3 @@ model.fit_generator(train_gen, steps_per_epoch = len(train) / batch_size, \
 
 print('Saving model to file...')
 model.save('steering.h5')
-
